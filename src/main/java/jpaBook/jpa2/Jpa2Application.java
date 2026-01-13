678eb1b646f4ac2a2157c4c3c9b724f04c4d4676
dmddjd <dmstjd99510@naver.com>
2026-01-13 19:24:22 +0900

    2026/01/13 item, order clear

--- src/main/java/jpaBook/jpa2/Jpa2Application.java
+++ src/main/java/jpaBook/jpa2/Jpa2Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Jpa2Application {
+public class
+Jpa2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Jpa2Application.class, args);
