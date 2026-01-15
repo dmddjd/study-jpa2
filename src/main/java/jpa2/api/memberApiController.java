63a95ad15b849065a576da34a457660c7edfb860
dmddjd <dmstjd99510@naver.com>
2026-01-15 20:00:04 +0900

    2026/01/15

---
+++ src/main/java/jpa2/api/memberApiController.java
@@ -0,0 +1,45 @@
+package jpa2.api;
+
+import jakarta.validation.Valid;
+import jpa2.domain.Member;
+import jpa2.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // @Controller + @ResponseBody
+@RequiredArgsConstructor
+public class memberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMEmberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+}
--- src/main/java/jpa2/domain/Member.java
+++ src/main/java/jpa2/domain/Member.java
@@ -1,6 +1,7 @@
 package jpa2.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+    @NotEmpty
     private String name;
     @Embedded
     private Address address;
